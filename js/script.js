4e639e8cb75a2361e8d645ef292df8a3e19265d1
Gabrielbg211 <Gbustosgarcia01@gmail.com>
2024-02-15 17:07:15 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Obtener referencia al botón de hamburguesa y al menú de navegación
+    const botonHamburguesa = document.querySelector('.boton-hamburguesa');
+    const menuNavegacion = document.querySelector('.navegacion');
+
+    // Agregar un evento de clic al botón de hamburguesa
+    botonHamburguesa.addEventListener('click', function () {
+        // Alternar la clase "abierto" en el botón de hamburguesa y en el menú de navegación
+        botonHamburguesa.classList.toggle('abierto');
+        menuNavegacion.classList.toggle('abierto');
+    });
+});
