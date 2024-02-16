f17eaf5de6d2876e011e81a3e4fc927b910c69cf
Gabrielbg211 <gbustosgarcia01@gmail.com>
2024-02-16 09:45:55 -0300

    Last Version

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     // Obtener referencia al botón de hamburguesa y al menú de navegación
     const botonHamburguesa = document.querySelector('.boton-hamburguesa');
     const menuNavegacion = document.querySelector('.navegacion');
 
     // Agregar un evento de clic al botón de hamburguesa
-    botonHamburguesa.addEventListener('click', function () {
+    botonHamburguesa.addEventListener('click', function() {
         // Alternar la clase "abierto" en el botón de hamburguesa y en el menú de navegación
         botonHamburguesa.classList.toggle('abierto');
         menuNavegacion.classList.toggle('abierto');
@@ -13,11 +13,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 document.addEventListener("DOMContentLoaded", function() {
-  setTimeout(function(){
-    document.getElementById('loader-wrapper').style.opacity = 0;
-    setTimeout(function(){
-      document.getElementById('loader-wrapper').style.display = 'none';
-      document.getElementById('content').style.display = 'block';
-    }, 5000);
-  }, 5000);
+    // Ocultar el loader y mostrar el contenido principal después de un tiempo
+    setTimeout(function() {
+        document.getElementById('loader-wrapper').style.opacity = 0; // Aplicar efecto de desvanecimiento
+        setTimeout(function() {
+            document.getElementById('loader-wrapper').style.display = 'none'; // Ocultar el loader
+            document.getElementById('content').style.display = 'block'; // Mostrar el contenido principal
+        }, 500); // Retardo para el efecto de desvanecimiento
+    }, 3000); // Retardo para mostrar el contenido principal, ajusta según tu preferencia
 });
