3fc40c33cf954ea79b207b483326eb2580386954
Gabrielbg211 <Gbustosgarcia01@gmail.com>
2024-02-20 23:44:16 -0300

    Last Version

--- js/script.js
+++ js/script.js
@@ -68,3 +68,17 @@ if (currentPage === "/Login.php") {
     const navegacion = document.getElementById("navegacion");
     navegacion.style.display = "none";
 }
+
+// Login
+
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
