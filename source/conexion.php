651ea4811b02be9aed76de79c87483d0489da566
Gabrielbg211 <Gbustosgarcia01@gmail.com>
2024-02-28 12:49:03 -0300

    DASHBOARD

--- source/conexion.php
+++ source/conexion.php
@@ -2,11 +2,11 @@
 session_start();
 
 // Definir las variables de conexión
-$DB_HOST = "localhost";
-$DB_USER = "gabriel";
-$DB_PASSWORD = "root";
-$DB_NAME = "caram_studio";
-$DB_PORT = 3306; // Puerto predeterminado para MySQL
+$servername = "roundhouse.proxy.rlwy.net";
+$username = "root";
+$password = "Ea6ge6CDfdAHaGF2E6DFb1FDA6EbDHEh";
+$database = "railway";
+$db_port = "30344";
 
 // Inicializar mensaje vacío
 $message = "";
@@ -19,7 +19,7 @@ if (isset($_SESSION['alert_displayed']) && $_SESSION['alert_displayed'] === true
     // Verificar si se enviaron datos del formulario
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Crear la conexión a la base de datos
-        $conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME, $DB_PORT);
+        $conn = mysqli_connect($servername, $username, $password, $database, $db_port);
 
         // Verificar la conexión
         if (!$conn) {
